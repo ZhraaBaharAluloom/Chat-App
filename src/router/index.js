6aa63d6eeda07079db153d2c4d2bad9967905ec8
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-06-06 21:56:48 +0300

    Add signin and signup routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import SignupVue from "../views/SignupView.vue";
+import SigninVue from "../views/SigninVue.vue";
 
 const routes = [
   {
+    name: "Home",
     path: "/",
     component: HomeView,
+  },
+  {
+    name: "Sign Up",
+    path: "/auth/signup",
+    component: SignupVue,
+  },
+  {
+    name: "Sign In",
+    path: "/auth/signin",
+    component: SigninVue,
   },
 ];
 
