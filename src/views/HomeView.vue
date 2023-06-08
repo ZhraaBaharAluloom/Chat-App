0253e862724f0d9e9f22ebd40febd7fa7135cc4b
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-06-08 11:25:39 +0300

    Move checkForToken function, add redirection router
    - Moved the checkForToken function to the home file for better organization.
    - Added a router to redirect users to the signin page if the token is null.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,73 +1,77 @@
 <template>
-  <div class="flex">
-    <div id="Header" class="fixed w-[420px] z-10">
-      <div
-        class="bg-[#F0F0F0] w-full flex justify-between items-center px-3 py-2"
-      >
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png"
-          alt="profile-image"
-          class="rounded-full ml-1 w-10"
-        />
-        <div class="flex items-center justify-center">
-          <AccountGroupIcon fillColor="#515151" class="mr-6" />
-          <DotsVerticalIcon fillColor="#515151" class="cursor-pointer" />
-        </div>
-      </div>
-
-      <div id="search" class="bg-white w-full px-2 border-b shadow-sm">
+  <div>
+    <div class="flex">
+      <div id="Header" class="fixed w-[420px] z-10">
         <div
-          class="px-1 m-2 flex items-center justify-center bg-[#F0F0F0] rounded-md"
+          class="bg-[#F0F0F0] w-full flex justify-between items-center px-3 py-2"
         >
-          <MagnifyIcon fillColor="#515151" :size="18" class="ml-2" />
-          <input
-            class="ml-5 appearance-non w-full bg-[#F0F0F0] py-1.5 px-2.5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder text-sm placeholder:text-gray-500"
-            autocomplete="off"
-            type="text"
-            placeholder="Start a new chat"
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png"
+            alt="profile-image"
+            class="rounded-full ml-1 w-10"
           />
+          <div class="flex items-center justify-center">
+            <AccountGroupIcon fillColor="#515151" class="mr-6" />
+            <DotsVerticalIcon fillColor="#515151" class="cursor-pointer" />
+          </div>
+        </div>
+
+        <div id="search" class="bg-white w-full px-2 border-b shadow-sm">
+          <div
+            class="px-1 m-2 flex items-center justify-center bg-[#F0F0F0] rounded-md"
+          >
+            <MagnifyIcon fillColor="#515151" :size="18" class="ml-2" />
+            <input
+              class="ml-5 appearance-non w-full bg-[#F0F0F0] py-1.5 px-2.5 text-gray-700 leading-tight focus:outline-none focus:shadow-outline placeholder text-sm placeholder:text-gray-500"
+              autocomplete="off"
+              type="text"
+              placeholder="Start a new chat"
+            />
+          </div>
         </div>
       </div>
-    </div>
-    <div v-if="isLoading">
-      <p class="mt-[100px]">Loading ...</p>
-    </div>
-    <div v-else>
-      <ChatView
-        class="mt-[100px]"
-        :chatsList="chatsList"
-        :openChat="openChat"
-      />
-    </div>
-    <div v-if="open">
-      <MessageView
-        :openedChat="openedChat"
-        @createMsg="createMsg"
-        @deleteMsg="deleteMsg"
-        @editMsg="editMsg"
-      />
-    </div>
+      <div v-if="isLoading">
+        <p class="mt-[100px]">Loading ...</p>
+      </div>
+      <div v-else>
+        <ChatView
+          class="mt-[100px]"
+          :chatsList="chatsList"
+          :openChat="openChat"
+          :user="user"
+        />
+      </div>
+      <div v-if="open">
+        <MessageView
+          :openedChat="openedChat"
+          @createMsg="createMsg"
+          @deleteMsg="deleteMsg"
+          @editMsg="editMsg"
+          :user="user"
+        />
+      </div>
 
-    <div v-else>
-      <div
-        class="ml-[420px] fixed w-[calc(100vw-420px)] h-[100vh] bg-gray-100 text-center"
-      >
-        <div class="grid h-screen place-items-center">
-          <div>
-            <div class="w-full flex items-center">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png"
-                alt="profile-image"
-                width="375"
-              />
-            </div>
+      <div v-else>
+        <div
+          class="ml-[420px] fixed w-[calc(100vw-420px)] h-[100vh] bg-gray-100 text-center"
+        >
+          <div class="grid h-screen place-items-center">
+            <div>
+              <div class="w-full flex items-center">
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png"
+                  alt="profile-image"
+                  width="375"
+                />
+              </div>
 
-            <div class="text-[32px] text-gray-500 font-light mt-10">
-              WhatsApp Web
-            </div>
+              <div class="text-[32px] text-gray-500 font-light mt-10">
+                WhatsApp Web
+              </div>
 
-            <div class="text-[14px] text-gray-500 font-light mt-1">
-              Welcome to the real world!
+              <div class="text-[14px] text-gray-500 font-light mt-1">
+                Welcome to the real world!
+              </div>
             </div>
           </div>
         </div>
@@ -78,13 +82,18 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
+import { useRouter } from "vue-router";
 
+import { setUser } from "../services/userUtils";
 import ChatView from "./ChatView.vue";
 import MessageView from "./MessageView.vue";
 
+// Icons
 import AccountGroupIcon from "vue-material-design-icons/AccountGroup.vue";
 import DotsVerticalIcon from "vue-material-design-icons/DotsVertical.vue";
 import MagnifyIcon from "vue-material-design-icons/Magnify.vue";
+
+import decode from "jwt-decode";
 import axios from "axios";
 
 /* TODO2:
@@ -95,6 +104,9 @@ let chatsList = ref([]);
 let open = ref(false);
 let openedChat = ref([]);
 let isLoading = ref(true);
+let user = ref(null);
+
+const router = useRouter();
 
 const getChats = async () => {
   try {
@@ -162,7 +174,20 @@ const openChat = (message) => {
   openedChat.value = message;
 };
 
+function checkForToken() {
+  const token = localStorage.getItem("myToken");
+  if (token) {
+    setUser(token);
+    user.value = decode(token);
+  } else {
+    localStorage.removeItem("myToken");
+    router.push("/auth/signin");
+    alert("Please try to login to continue");
+  }
+}
+
 onMounted(async () => {
   await getChats();
+  await checkForToken();
 });
 </script>
