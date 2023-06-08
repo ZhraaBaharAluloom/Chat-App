fd1bcccf2edbf3f6c0d2d2ac5898e1cdc79db7ba
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-06-08 11:23:55 +0300

    Remove unnecessary code
    Refactor utils file and move checkForToken function to the home file

--- src/services/userUtils.js
+++ src/services/userUtils.js
@@ -1,26 +1,15 @@
 import axios from "axios";
 import decode from "jwt-decode";
 
-export let user = null;
-
 export function setUser(token) {
+  // let receivedToken;
+  // let receivedUser;
+  // [...args].map((arg) => {
+  //   arg.token ? (receivedToken = arg.token) : (receivedUser = arg.user);
+  // });
+  // axios.defaults.headers.common.Authorization = `Bearer ${receivedToken}`;
+  // localStorage.setItem("myToken", receivedToken);
+  // receivedUser = decode(receivedToken);
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
   localStorage.setItem("myToken", token);
-  user = decode(token);
-  return user;
-}
-
-export function checkForToken() {
-  const token = localStorage.getItem("myToken");
-  if (token) {
-    const currentTime = Date.now() / 1000;
-    const user = decode(token);
-    if (user.exp >= currentTime) {
-      setUser(token);
-    } else {
-      alert("Your session is expired, please try to login again");
-    }
-  } else {
-    alert("Your session is expired, please try to login again");
-  }
 }
