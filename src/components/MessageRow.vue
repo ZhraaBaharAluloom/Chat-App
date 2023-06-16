9d59d081faa776c3897f0d606a0c923f0c6b5535
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-06-16 11:02:35 +0300

    Refactor MessageRow.vue for improved functionality
    - Modified getRoomId method to accept roomId as a parameter.
    - Filtered chatsList to obtain partner user's username, profile image, and chats.
    - Created computed property computeNotEnrolledUsers for generating a list of non-enrolled users.
    - Updated notEnrolledUsers reference on computeNotEnrolledUsers changes.
    - Added comments to enhance code clarity.

--- src/components/MessageRow.vue
+++ src/components/MessageRow.vue
@@ -2,20 +2,29 @@
   <div class="">
     <div
       class="flex w-full px-4 py-3 items-center cursor-pointer"
-      @click="openChat(user?.id)"
+      @click="getRoomId(room?.id)"
     >
       <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/2048px-Circle-icons-profile.svg.png"
+        :src="
+          filteredChats.find((chat) => chat.user.id !== enrolledUser.id)?.user
+            .profileImg || notEnrolledUsers[0].profileImg
+        "
         alt="profile-image"
         class="rounded-full mr-4 w-12"
       />
       <div class="w-full">
         <div class="flex justify-between items-center">
           <div class="text-[15px] text-gray-600">
-            {{ user?.username || "Zahraa" }}
+            {{
+              filteredChats.find((chat) => chat.user.id !== enrolledUser.id)
+                ?.user?.username || notEnrolledUsers[0].username
+            }}
           </div>
-          <div class="text-[12px] text-gray-600 fixed left-56">
-            {{ chatsList[chatsList.length - 1].formattedCreatedDate }}
+          <div
+            v-if="filteredChats.length > 0"
+            class="text-[12px] text-gray-600 fixed left-56"
+          >
+            {{ filteredChats[filteredChats.length - 1].formattedCreatedDate }}
           </div>
         </div>
         <div class="flex items-center">
@@ -23,8 +32,8 @@
           <div
             class="text-[15px] text-gray-500 flex items-center justify-between w-full"
           >
-            <p class="line-clamp-1">
-              {{ chatsList[chatsList.length - 1].text }}
+            <p class="line-clamp-1" v-if="filteredChats.length > 0">
+              {{ filteredChats[filteredChats.length - 1].text }}
             </p>
           </div>
         </div>
@@ -35,17 +44,67 @@
 </template>
 
 <script setup>
+import { computed, onUpdated, ref, watch } from "vue";
 import CheckAllIcon from "vue-material-design-icons/CheckAll.vue";
 
 const props = defineProps({
-  chatsList: {
-    type: Array,
-  },
   openChat: {
     type: Function,
   },
-  user: {
+  room: {
     type: Object,
   },
+  chatsList: {
+    type: Array,
+  },
+  roomChatsList: {
+    type: Array,
+  },
+  enrolledUser: {
+    type: Object,
+  },
+  usersList: {
+    type: Array,
+  },
+});
+
+const filteredChats = computed(() => {
+  return props.chatsList.filter((chat) => chat.room?.id === props.room.id);
+});
+
+const notEnrolledUsers = ref([]);
+
+// Compute the list of not enrolled users based on props
+const computeNotEnrolledUsers = computed(() => {
+  return props.usersList.filter((user) => {
+    return (
+      user.id !== props.enrolledUser.id &&
+      user.chatRooms.some((room) => {
+        return room.id === props.room.id;
+      })
+    );
+  });
+});
+
+// Update the notEnrolledUsers reference when computeNotEnrolledUsers changes
+const updateNotEnrolledUsers = () => {
+  notEnrolledUsers.value = computeNotEnrolledUsers.value;
+};
+
+// Watch for changes in props and update the notEnrolledUsers reference accordingly
+watch(
+  [() => props.usersList, () => props.enrolledUser, () => props.receivedRoomId],
+  updateNotEnrolledUsers,
+  { immediate: true }
+);
+
+const emits = defineEmits(["openChat"]);
+
+const getRoomId = (roomId) => {
+  emits("openChat", roomId);
+};
+
+onUpdated(() => {
+  return filteredChats;
 });
 </script>
